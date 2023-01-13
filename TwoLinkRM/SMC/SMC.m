9509221a6f8ef1db05b775f2c9c2b4022cad533a
Ram Padmanabhan <rampadmanabhan0609@gmail.com>
2023-01-13 15:25:12 -0500

    SMC Changes
    Changes based on the second round of review - just added ddot(theta) last time.

--- TwoLinkRM/SMC/SMC.m
+++ TwoLinkRM/SMC/SMC.m
@@ -1,12 +1,14 @@
 function out = SMC(u)
-global K_D epsilon
-D = [15 0; 0 10];
-qe = [u(1); u(3)];
-qe_dot = [u(2); u(4)];
-s = qe_dot +  K_D*qe;
-if norm(s) > epsilon
-    out = D*s/norm(s);
-else
-    out = D*s/epsilon;
-end
+    global K_D epsilon
+
+    D = [15 0; 0 10];
+    qe = [u(1); u(3)];
+    qe_dot = [u(2); u(4)];
+    xmdot = [u(6); u(8)];
+    s = qe_dot +  K_D*qe;
+    if norm(s) > epsilon
+        out = D*s/norm(s) + xmdot;
+    else
+        out = D*s/epsilon + xmdot;
+    end
 end
